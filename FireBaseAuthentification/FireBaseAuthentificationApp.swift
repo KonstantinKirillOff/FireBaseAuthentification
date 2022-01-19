1ec5afbf9573f0c147913b80186bdd2f98b21b7f
kirillOff <kirilloffkostya@gmail.com>
2022-01-19 17:55:44 +0300

    Initial Commit

---
+++ FireBaseAuthentification/FireBaseAuthentificationApp.swift
@@ -0,0 +1,17 @@
+//
+//  FireBaseAuthentificationApp.swift
+//  FireBaseAuthentification
+//
+//  Created by Konstantin Kirillov on 19.01.2022.
+//
+
+import SwiftUI
+
+@main
+struct FireBaseAuthentificationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
